59abac1535d86d29a119274f122ff36333b1eb2b
Byron 2016 <bgva2005@yahoo.com>
2021-11-12 18:22:16 -0500

    001_components Add styles color and size al componente ATOMICO Heading

--- components/atomDesign/atoms/Heading/Heading.js
+++ components/atomDesign/atoms/Heading/Heading.js
@@ -3,12 +3,26 @@ import PropTypes from 'prop-types';
 import classnames from 'classnames';
 import './Heading.css';
 
-const Heading = ({ children}) => (
-  <heading className={classnames('heading')}>{children}</heading>
+export const SIZES = ['xs', 'sm', 'md'];
+
+const Heading = ({ children, color, size}) => (
+  <h1 className={classnames(
+    'heading', { 
+      [`color-${color}`]: color,
+      [`size-${size}`]: size 
+    })}>{children}</h1>
 );
 
 Heading.prototypes = {
   children: PropTypes.node.isRequired,
+   size: PropTypes.oneOf(['xs', 'sm', 'md']),
+   color: PropTypes.oneOf(['primary']),
 }
+
+Heading.defaultProps = {
+  children: "ContenidoDefault",
+  size: SIZES[0],
+  color: "default",
+};
 
 export default Heading;
--- components/atomDesign/atoms/Heading/Heading.stories.jsx
+++ components/atomDesign/atoms/Heading/Heading.stories.jsx
@@ -1,15 +1,47 @@
-import Heading from './Heading';
+import React from 'react';
+import Heading, { SIZES } from './Heading';
 
 export default {
   component: Heading,
   title: "Componentes/Heading",
+  argTypes: {
+    size: {
+      options: SIZES,
+      control: { type: 'radio' }
+    }
+  }
 };
 
 const Template = (args) => <Heading {...args}/>;
 
-export const Primary = Template.bind({});
-Primary.args = {
-  children: 'Primary'
+export const Default = Template.bind({});
+Default.args = {
+  children: 'Default text',
+  size: SIZES[0],
+  color: "default",
 };
+
+export const PrimaryStraSmall = Template.bind({});
+PrimaryStraSmall.args = {
+  children: 'Primary text',
+  size: SIZES[0],
+  color: "primary",
+};
+
+export const PrimarySmall = Template.bind({});
+PrimarySmall.args = {
+  children: 'Small text',
+  size: SIZES[1],
+  color: "primary",
+};
+
+export const PrimaryMedium = Template.bind({});
+PrimaryMedium.args = {
+  children: 'Medium text',
+  size: SIZES[2],
+  color: "primary",
+};
+
+
 
 
