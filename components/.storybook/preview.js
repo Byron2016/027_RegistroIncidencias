651096f398379c05e9d5edf67910745689ec6615
Byron 2016 <bgva2005@yahoo.com>
2021-11-12 11:04:42 -0500

    001_components Agregar carpeta nueva para storybook y definir previews

--- components/.storybook/preview.js
+++ components/.storybook/preview.js
@@ -1,9 +1,35 @@
+import { INITIAL_VIEWPORTS } from "@storybook/addon-viewport";
+import { MINIMAL_VIEWPORTS } from "@storybook/addon-viewport";
+const customViewports = {
+  kindleFire2: {
+    name: "BV Kindle Fire 2",
+    styles: {
+      width: "600px",
+      height: "963px",
+    },
+  },
+  kindleFireHD: {
+    name: "BV Kindle Fire HD",
+    styles: {
+      width: "533px",
+      height: "801px",
+    },
+  },
+};
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
+    },
+  },
+  viewport: {
+    viewports: {
+      ...MINIMAL_VIEWPORTS,
+      ...customViewports,
+      ...INITIAL_VIEWPORTS,
     },
   },
 }
