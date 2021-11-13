b99167a744880af7811522464fb5ddad275ac0c5
Byron 2016 <bgva2005@yahoo.com>
2021-11-13 13:40:12 -0500

    001_components V6 Primera aproximación RECURSIVA para convertir js a css

--- components/scripts/build-custom-properties.js
+++ components/scripts/build-custom-properties.js
@@ -4,52 +4,133 @@ const fs = require('fs');
 
 //console.log('>>>scipt')
 
+function transformTokens(parentKey, object){
+  
+  const objectkeys = Object.keys(object)
+
+  return objectkeys.reduce((tokensTransformed, objectkey) => {
+    const value = object[objectkey]
+
+    if(typeof value == 'object'){
+      return `
+        ${tokensTransformed}
+        --${transformTokens(`${parentKey}-${objectkey}`, value)}
+      `
+    }
+
+    return `
+      ${tokensTransformed}
+      --${parentKey}-${objectkey}: ${value};
+    `
+
+  }, '')
+
+  
+}
+
 function buildCustomProperties(){
-  const choicesKeys = Object.keys(choices)
-
-  // choicesKeys.reduce((prev, curr) => {
-
-  // }, '')
-
-  // const customProperties = `${JSON.stringify(choices)} ${JSON.stringify(decisions)}`;
-
-  let choicesStr = ''
-
-  if(typeof choices['colors'] == 'object'){
-    const colorkeys = Object.keys(choices['colors'])
-
-    choicesStr = colorkeys.reduce((prev, curr) => {
-      if(typeof choices['colors'][curr] == 'object') {
-        const brandkeys = Object.keys(choices['colors'][curr])
-        const colorsStr = brandkeys.reduce((prevBrandkeys, currBrandkeys) => {
-          const value = choices['colors'][curr][currBrandkeys]
+  const caso = 'Recursivo'
+  let customProperties_Final = ''
+  
+  if(caso == 'NoRecursivo'){
+    const choicesKeys = Object.keys(choices)
+    let choicesStr = ''
+    if(typeof choices['colors'] == 'object'){
+      const colorkeys = Object.keys(choices['colors'])
+      choicesStr = colorkeys.reduce((prev, curr) => {
+        if(typeof choices['colors'][curr] == 'object'){
+          const brandkeys = Object.keys(choices['colors'][curr])
+          const colorsStr = brandkeys.reduce((prevBrandkeys, currBrandkeys) =>{
+            const value = choices['colors'][curr][currBrandkeys]
+            return `
+              ${prevBrandkeys}
+              --colors-${curr}-${currBrandkeys}: ${value};
+            `;
+          },'')
+          //return brandkeys;
           return `
-          ${prevBrandkeys}
-          --colors-${curr}-${currBrandkeys}: ${value};
-          `;
-        }, '')
-        //return brandkeys;
-        return `
-        ${prev}
-        ${colorsStr}
-        `
-      } else {
-        return `
-        ${prev}
-        --colors-${curr}: ${choices['colors'][curr]};
-        `
-      }
-     }, '')
-  }
+            ${prev}
+            ${colorsStr}
+          `
+        } else {
+          return `
+            ${prev}
+            --colors-${curr}: ${choices['colors'][curr]};
+          `
+        }
+      },'')
+    }
+    //const customProperties = choicesKeys
+    const customProperties = choicesStr
+    customProperties_Final = customProperties
+    // const data = `
+    //   :root {
+    //     ${customProperties}
+    //   }
+    // `
 
-  //const customProperties = choicesKeys
-  const customProperties = choicesStr
+
+  }
+  if(caso == 'Recursivo'){
+    const choicesKeys = Object.keys(choices)
+    const choicesStr = transformTokens('colors', choices.colors)
+    const customProperties = choicesStr
+    customProperties_Final = customProperties
+    // const data = `
+    //   :root {
+    //     ${customProperties}
+    //   }
+    // `
+  }
+  // const choicesKeys = Object.keys(choices)
+  // let choicesStr = ''
+  // if(typeof choices['colors'] == 'object'){
+  //   const colorkeys = Object.keys(choices['colors'])
+
+  //   choicesStr = colorkeys.reduce((prev, curr) => {
+  //     if(typeof choices['colors'][curr] == 'object') {
+  //       const brandkeys = Object.keys(choices['colors'][curr])
+  //       const colorsStr = brandkeys.reduce((prevBrandkeys, currBrandkeys) => {
+  //         const value = choices['colors'][curr][currBrandkeys]
+  //         return `
+  //         ${prevBrandkeys}
+  //         --colors-${curr}-${currBrandkeys}: ${value};
+  //         `;
+  //       }, '')
+  //       //return brandkeys;
+  //       return `
+  //       ${prev}
+  //       ${colorsStr}
+  //       `
+  //     } else {
+  //       return `
+  //       ${prev}
+  //       --colors-${curr}: ${choices['colors'][curr]};
+  //       `
+  //     }
+  //   }, '')
+  // }
+
+  // //const customProperties = choicesKeys
+  // const customProperties = choicesStr
+
+  // const data = `
+  // :root {
+  //   ${customProperties}
+  // }
+  // `
+  // fs.writeFile('./util/tokens.css', data, 'utf8', function(error){
+  //   if(error){
+  //     return console.error(error);
+  //   }
+  // });
 
   const data = `
-  :root {
-    ${customProperties}
-  }
-  `
+    :root {
+      ${customProperties_Final}
+    }
+    `
+
   fs.writeFile('./util/tokens.css', data, 'utf8', function(error){
     if(error){
       return console.error(error);
