b0e8116a4966d74d6d9b92b1a39ec30fa7319ae4
Byron 2016 <bgva2005@yahoo.com>
2021-11-17 11:43:19 -0500

    001_components V11 Agregar Componente DrowDown

---
+++ components/atomDesign/molecules/Dropdown/Dropdown.stories.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Dropdown from './Dropdown'
+
+export default {
+  title: 'Molecules/Dropdown',
+  component: Dropdown,
+}
+
+export const Default = () => <Dropdown />
