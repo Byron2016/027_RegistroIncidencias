5b6f2f21ea86c1649ca09c15392479687d0a473a
Byron 2016 <bgva2005@yahoo.com>
2021-11-17 14:16:02 -0500

    001_components V12 Componente DropDown select dinámico x función map

--- components/atomDesign/molecules/Dropdown/Dropdown.js
+++ components/atomDesign/molecules/Dropdown/Dropdown.js
@@ -1,21 +1,54 @@
 import React from 'react'
-// import PropTypes from 'prop-types'
+import PropTypes from 'prop-types'
 import Icon from '../../atoms/Icons'
 
 import './Dropdown.css'
 
-const Dropdown = () => (
+export const OPTIONS = [
+  {
+    text: '9:00 AM',
+    value: 800,
+  },
+  {
+    text: '10:00 AM',
+    value: 1000,
+  },
+  {
+    text: '1:00 PM',
+    value: 1300,
+  },
+  {
+    text: '3:00 PM',
+    value: 1500,
+  },
+]
+
+const Dropdown = ({ options, value, onChange }) => (
   <div className="dropdown">
     <Icon type="angleDown" hasBackground />
-    <select className="dropdown-select">
-      <option>8:00 AM</option>
-      <option>9:00 AM</option>
+    <select className="dropdown-select" onChange={onChange} value={value}>
+      {options.map(({ text, value }) => (
+        <option key={value} value={value}>
+          {text}
+        </option>
+      ))}
     </select>
   </div>
 )
 
-// Dropdown.propTypes = {
-//   value: PropTypes.string
-// }
+Dropdown.propTypes = {
+  value: '',
+}
+
+Dropdown.propTypes = {
+  onChange: PropTypes.func.isRequired,
+  options: PropTypes.arrayOf(
+    PropTypes.shape({
+      text: PropTypes.string.isRequired,
+      value: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+  value: PropTypes.string,
+}
 
 export default Dropdown
