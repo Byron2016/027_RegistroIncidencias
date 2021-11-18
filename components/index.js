f26295d675c9bcd9cbebd3cfbda9668027846517
Byron 2016 <bgva2005@yahoo.com>
2021-11-18 14:56:41 -0500

    001_components V13 creación index.js actualización package.json

---
+++ components/index.js
@@ -0,0 +1,15 @@
+// Layout
+import Spacer from './atomDesign/layout/Spacer'
+
+// Atoms
+import Button from './atomDesign/atoms/Button'
+import Heading from './atomDesign/atoms/Heading'
+import Icon from './atomDesign/atoms/Icons'
+import Input from './atomDesign/atoms/Input'
+import Picture from './atomDesign/atoms/Picture'
+
+// Molecules
+import ButtonIcon from './atomDesign/molecules/ButtonIcon'
+import Dropdown from './atomDesign/molecules/Dropdown'
+
+export { Spacer, Button, Heading, Icon, Input, Picture, ButtonIcon, Dropdown }
