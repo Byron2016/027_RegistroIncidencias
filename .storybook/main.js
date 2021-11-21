ca54688083f7b5a2e8516dc859cfb699154a1b29
Byron 2016 <bgva2005@yahoo.com>
2021-11-21 18:17:07 -0500

    002_ccsmodule Instalación paquete storybook-css-modules-preset

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,12 +1,13 @@
 module.exports = {
-  "stories": [
-    "../atomDesign/**/*.stories.@(js|jsx|ts|tsx)",
+  stories: [
+    '../atomDesign/**/*.stories.@(js|jsx|ts|tsx)',
     // "../ejemplos/**/*.stories.@(js|jsx|ts|tsx)",
     // "../stories/**/*.stories.mdx",
     // "../stories/**/*.stories.@(js|jsx|ts|tsx)"
   ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials"
-  ]
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    'storybook-css-modules-preset',
+  ],
 }
